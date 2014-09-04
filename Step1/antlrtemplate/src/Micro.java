e709b33cafe65f8b1455dc0b42cb9a700b6b2155
abahrain <abahrain@purdue.edu>
2014-09-04 15:14:51 -0400

    cleaned up and commented

--- Step1/antlrtemplate/src/Micro.java
+++ Step1/antlrtemplate/src/Micro.java
@@ -5,16 +5,20 @@ public class Micro
 {
   public static void main(String[] args) throws Exception
   {
-      //ANTLR will read in the file
+      //ANTLR will read in the file and pass the string to the lexer
       ANTLRFileStream stream = new ANTLRFileStream(args[0]);
       Micro_scan lex = new Micro_scan(stream);
+	  //The lexer piece is then set equal to a token
       Token t = lex.nextToken();
       
+	  //Runs until the end of the file
+	  //prints the token type and value to the screen dump
+	  //gets next token in the set
       while(t.getType() != Micro_scan.EOF)
       {
           System.out.println("Token Type: "+Micro_scan.tokenNames[t.getType()]);
           System.out.println("Value: "+t.getText());
-	  t = lex.nextToken();
+		  t = lex.nextToken();
       }
   }
 }
